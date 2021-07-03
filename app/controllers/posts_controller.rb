3fc67b49cb32ba8271d7dbfaa5fb71d2628e3019
carlos daniel hernandez rodriguez <a20160383@gmail.com>
2021-07-03 16:28:57 -0500

    create and edit post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,13 @@
-class PostController < ApplicationController
+class PostsController < ApplicationController
   # GET /post
   def index
     @posts = Post.where(published: true)
     render json: @posts, status: :ok
   end
+
   # GET /post/{id}
   def show
     @post = Post.find(params[:id])
-    render json: @json, status: :ok
+    render json: @post, status: :ok
   end
 end
