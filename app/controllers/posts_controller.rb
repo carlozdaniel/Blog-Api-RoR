1763527ab1b5e226fb9241e93f30a4f214451bb0
carlos daniel hernandez rodriguez <a20160383@gmail.com>
2021-07-04 23:17:09 -0500

    add routes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,9 +5,37 @@ class PostsController < ApplicationController
     render json: @posts, status: :ok
   end
 
-  # GET /post/{id}
+  # GET /posts/{id}
   def show
     @post = Post.find(params[:id])
     render json: @post, status: :ok
   end
+
+  # GET /posts/
+  def create
+    @post = Post.create!(create_params)
+    render json: @post, status: :created
+  end
+
+  # PUT /posts/{id}
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update!(update_params)
+    render json: @post, status: :ok
+  end
+
+  private
+
+  def create_params
+    params.require(:post).permit(:title, :content, :published, :user_id)
+  end
+
+  def update_params
+    params.require(:post).permit(:title, :content, :published)
+  end
 end
+
+
+
+
